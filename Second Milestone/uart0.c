baf3ad98ad0f7a5dbe30465ad790da5d885aecf5
KareemHesham1 <168685301+KareemHesham1@users.noreply.github.com>
2024-05-16 23:19:36 +0300

    Update uart0.c

--- Second Milestone/uart0.c
+++ Second Milestone/uart0.c
@@ -24,18 +24,3 @@ void UART0Init(void){
     GPIO_PORTA_DEN_R |= 0X03; //digital enable
 }
 
-void UART7Init(void){
-    SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R7;
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;
-
-    UART7_CTL_R &= ~0x01; //disable uart
-    UART7_IBRD_R = 104 ; //integer baud rate divisor
-    UART7_FBRD_R = 11 ; //float baud rate divisor
-    UART7_LCRH_R |= 0x70 ; //fifo enable and width  8 bits
-    UART7_CTL_R = 0X0301 ; //uart enable , rx enable , tx enable
-    GPIO_PORTE_DEN_R |= 0X03; //digital enable
-    GPIO_PORTE_AMSEL_R &= ~0x03; //clear analog mode
-    GPIO_PORTE_AFSEL_R |= 0x03; //set alternate function
-    GPIO_PORTE_PCTL_R = (GPIO_PORTE_PCTL_R &= ~0xFF) | 0X00000011; //clear PE0 and PE1 then set them as uart
-
-}
