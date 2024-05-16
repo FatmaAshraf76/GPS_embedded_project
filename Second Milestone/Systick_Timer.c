dba10b166e7b69f14e47edcddedfed6c6b36b2a9
OmarAboBakr7 <89665983+OmarAboBakr7@users.noreply.github.com>
2024-05-16 23:26:32 +0300

    Update Systick_Timer.c

--- Second Milestone/Systick_Timer.c
+++ Second Milestone/Systick_Timer.c
@@ -1,11 +1,12 @@
 #include "tm4c123gh6pm.h"
-#include "string.h"
-#include "stdint.h"
-#include "math.h"
-#include "stdlib.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "stdio.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "Systick_Timer.h"
 
 
 // systick timer
